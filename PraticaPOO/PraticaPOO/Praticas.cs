a90786c93c2b9dd9cb8757a5e8b8c996eebfe266
jv_m13@hotmail.com <jv_m13@hotmail.com>
2015-05-09 16:54:30 -0300

    Primeiro Commit

---
+++ PraticaPOO/PraticaPOO/Praticas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticaPOO
+{
+    class Praticas
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
