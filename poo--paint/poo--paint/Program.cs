cdb02184882ba6090ab6ee6b3c11913275e272a8
jv_m13@hotmail.com <jv_m13@hotmail.com>
2015-05-09 17:19:57 -0300

    Criação do projeto Poo-paint - Atividade 1

---
+++ poo--paint/poo--paint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo__paint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        /*Crie um novo projeto chamado poo-paint, dentro do reposit´orio poo-2015. • Crie a classe com nome Retangulo, contendo os seguintes 
+            membros: – Atributos x e y para representar a posic¸˜ao do retˆangulo na tela. – Atributos largura e altura para representar as 
+        dimens˜oes do retˆangulo. – Um construtor que recebe os quatro parˆametros acima e inicializa os atributos correspondentes. – Um 
+            m´etodo Imprime que retorna uma String que descreve um retˆangulo (para imprimir no console, por exemplo). Um retˆangulo na 
+                posi¸c˜ao (x,y) = (34,20), 300 de largura e 40 de altura deve ser representado pela seguinte String: 
+        retangulo[x:34,y:20,l:300,a:40]*/
+    }
+}
